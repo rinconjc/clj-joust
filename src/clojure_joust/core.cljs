0f74ffb3a10a3e04ba3b0ebd722a9656e179e900
Julio Rincon <jcrinconc@gmail.com>
2019-08-28 19:15:07 +1000

    updated

--- src/clojure_joust/core.cljs
+++ src/clojure_joust/core.cljs
@@ -14,35 +14,55 @@
                           :player2 {:avatar :bear}}))
 
 (def avatars {:lion "https://blog.udemy.com/wp-content/uploads/2014/04/shutterstock_15856930.jpg"
-              :bear "https://www.exploringnature.org/wordpress/wp-content/uploads/2015/01/bear_standing_up.jpg"})
+              :bear "https://www.exploringnature.org/wordpress/wp-content/uploads/2015/01/bear_standing_up.jpg"
+              :bandicoot "http://pre08.deviantart.net/0088/th/pre/i/2016/092/2/f/crash_bandicoot_smashified__by_sean_the_artist-d9xfq0i.png"
+              :lycanroc "https://orig00.deviantart.net/68dc/f/2017/219/e/a/lycanroc_midnight_dusk_form_2_by_tarun2207-dbj6fgh.png"})
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
+(defn player [name avatar]
+  [:div
+   [:img.character-pick {:src (avatars avatar)}]
+   [:a.left [:i.material-icons "chevron_left"]]
+   [:a.right [:i.material-icons "chevron_right"]]
+   [:span name]])
+
 (defn player-selection []
   [:div.row
    [:div.col.s5.m3.offset-m2.white
-    [:img.character-pick
-     {:src ""}]
-    [:a.left.arrow [:i.material-icons "chevron_left"]]
-    [:a.arrow.right [:i.material-icons "chevron_right"]]
-    [:span "Player 1"]]
+    [player "player 1" :bandicoot]]
    [:div.col.s2.center.middle.full-h "VS"]
    [:div.col.s5.m3.white
-    [:img.character-pick
-     {:src ""}]
-    [:span "Player 2"]]])
+    [player "player 2" :lycanroc]]
+   [:div.col.s12
+    [:div.center [:a.btn {:on-click #(swap! app-state assoc :page :arena)} "Start"]]]])
+
+(defn game-arena []
+  [:div.box.center
+   [:h2 "Jousting Arena"]
+   [:div.arena.content
+    [:canvas {:id "arena"}]
+    [:div.platform]]
+   ])
+
+(defn setup [])
+
+(def pages {:players #'player-selection
+            :arena #'game-arena})
 
 (defn main-page []
-  [:div.container
+  [:div.box
    [:h4 (:text @app-state)
     [:img {:src "icon.png"}]]
-   [player-selection]])
+   [(-> @app-state :page pages)]
+   ])
 
 (defn mount [el]
   (reagent/render-component [main-page] el))
 
 (defn mount-app-element []
+  (swap! app-state update :page #(or % :players))
   (when-let [el (get-app-element)]
     (mount el)))
 
